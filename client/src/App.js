1c0ae0797951b3c1595710821462d70adc35bd81
George Tupling <georgetupling@live.co.uk>
2023-03-18 19:01:32 +0000

    separate client and server dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,24 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Home from "./pages/Home";
 import ErrorPage from "./pages/ErrorPage";
+import LogIn from "./pages/LogIn";
+import Register from "./pages/Register";
 import MenuAppBar from "./components/MenuAppBar";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/login",
+    element: <LogIn />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
     errorElement: <ErrorPage />,
   },
 ]);
